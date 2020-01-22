8e0882c962dc48735662ef27a6db8864b4101644
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-22 16:15:41 -0300

    crut completo

--- dist/js/usuarios.js
+++ dist/js/usuarios.js
@@ -34,7 +34,8 @@ new Vue({
         email: this.datosUsuarios.email,
         telefono: this.datosUsuarios.telefono,
         password: this.datosUsuarios.password,
-        id: this.datosUsuarios.id
+        id: this.datosUsuarios.id,
+        c_password: this.datosUsuarios.c_password
       })
         .then(response => {
           this.cargarTabla();
@@ -42,8 +43,47 @@ new Vue({
         .catch(err => {
           console.log(err);
         })
+    },
+
+    verUsuario(id) {
+      axios.get(this.url + "api/usuarios/" + id)
+
+        .then(response => {
+          console.log(response.data[0]);
+          this.datosUsuarios = response.data[0];
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    },
+
+    editarUsuario() {
+      axios.put(this.url + "api/usuarios/" + this.datosUsuarios.id, {
+        nombres: this.datosUsuarios.nombres,
+        apellidos: this.datosUsuarios.apellidos,
+        rol: this.datosUsuarios.rol,
+        rut: this.datosUsuarios.rut,
+        email: this.datosUsuarios.email,
+        telefono: this.datosUsuarios.telefono,
+        password: this.datosUsuarios.password,
+      
+        c_password: this.datosUsuarios.c_password
+      })
+      .then(response => {
+        console.log(response);
+        this.cargarTabla();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    },
+
+    eliminarUsuario(id){
+      axios.delete(this.url + "api/usuarios/" + id)
+      .then(response => {
+        console.log(response);
+        this.cargarTabla();
+      })
     }
-
-
   }
 });
